bd728194e39b7a2b69274b78028484f893ef6469
Mr Blint <al844976@wcupa.edu>
2018-02-24 18:46:47 -0500

    ayylmao

--- stream.py
+++ stream.py
@@ -1,5 +1,6 @@
-import cv2
-import time
+#Author: Alexander Leventhal
+import os, cv2, time, base64, requests
+url = 'http://ff84.wcupa.network:1337/'
 ip = "rtsp://192.168.1.246:554/11.h264"
 vcap = cv2.VideoCapture(ip)
 count=1
@@ -8,5 +9,11 @@ while(1):
     ret, frame = vcap.read()
     if(count % 30 == 0):
         cv2.imwrite(str(count)+".jpg", frame)
+        image = open(str(count)+'.jpg', 'rb')
+        image_raw = image.read()
+        files = {'media': base64.encodestring(image_raw)}
+        requests.post(url, files=files)
+        #os.remove(str(count)+'.jpg')
     cv2.waitKey(1)
     count+=1
+    
