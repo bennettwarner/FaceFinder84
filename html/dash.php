a9929ed91f3b345dbf95637ba96867308410a06b
Flamelawd <36795443+Flamelawd@users.noreply.github.com>
2018-02-24 17:20:09 -0500

    Update dash.php

--- html/dash.php
+++ html/dash.php
@@ -1,7 +1,7 @@
-<?php require "login/loginheader.php"; ?>
-<?php include "login/dbconfig.php"; ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php require "login/loginheader.php"; ?> 
+<?php include "login/dbconfig.php"; ?> 
+<!DOCTYPE html> 
+<html lang="en"> 
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
@@ -13,11 +13,11 @@
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
         <link href="css/styles.css" rel="stylesheet">
-    </head>
-    <body>
-        <!-- header -->
-        <div id="top-nav" class="navbar navbar-inverse navbar-static-top">
-            <div class="container-fluid">
+    </head>     
+    <body> 
+        <!-- header -->         
+        <div id="top-nav" class="navbar navbar-inverse navbar-static-top"> 
+            <div class="container-fluid"> 
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="icon-bar"></span>
@@ -25,9 +25,9 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="#">FaceFinder 84™</a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
+                </div>                 
+                <div class="navbar-collapse collapse"> 
+                    <ul class="nav navbar-nav navbar-right"> 
                         <li class="dropdown">
                             <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> <?php echo "Blue" ?> <span class="caret"></span></a>
                             <ul id="g-account-menu" class="dropdown-menu" role="menu">
@@ -35,83 +35,83 @@
                                     <a href="./account.php">Account Settings</a>
                                 </li>
                             </ul>
-                        </li>
+                        </li>                         
                         <li>
                             <a href="login/logout.php"><i class="glyphicon glyphicon-lock"></i> Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <!-- /container -->
-        </div>
-        <!-- /Header -->
-        <!-- Main -->
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-3">
-                    <!-- Left column -->
-                    <a href="#"><strong><i class="glyphicon glyphicon-wrench"></i> Tools</strong></a>
-                    <hr>
-                    <ul class="nav nav-stacked">
-                        <ul class="nav nav-stacked collapse in" id="userMenu">
+                        </li>                         
+                    </ul>                     
+                </div>                 
+            </div>             
+            <!-- /container -->             
+        </div>         
+        <!-- /Header -->         
+        <!-- Main -->         
+        <div class="container-fluid"> 
+            <div class="row"> 
+                <div class="col-sm-3"> 
+                    <!-- Left column -->                     
+                    <a href="#"><strong><i class="glyphicon glyphicon-wrench"></i> Tools</strong></a> 
+                    <hr> 
+                    <ul class="nav nav-stacked"> 
+                        <ul class="nav nav-stacked collapse in" id="userMenu"> 
                             <li class="active">
                                 <a href="/dash.php"><i class="glyphicon glyphicon-home"></i> Home</a>
-                            </li>
+                            </li>                             
                             <li>
                                 <a href="#"><i class="glyphicon glyphicon-search"></i> New Case <span class="badge badge-info"></span></a>
-                            </li>
+                            </li>                             
                             <li>
                                 <a href="#"><i class="glyphicon glyphicon-list"></i> Active Cases</a>
-                            </li>
+                            </li>                             
                             <li>
                                 <a href="#"><i class="glyphicon glyphicon-tasks"></i> Jobs</a>
-                            </li>
+                            </li>                             
                             <li></li>
                             <li>
                                 <a href="#"><i class="glyphicon glyphicon-flag"></i> Audit</a>
                             </li>
                             <li>
-                                <a href="./account.php"><i class="glyphicon glyphicon-user"></i> Account</a>
-                            </li>
-                            <li></li>
+                                <a href="#"><i class="glyphicon glyphicon-user"></i>&nbsp;Account</a>
+                            </li>                             
+                            <li></li>                             
                             <li>
-                                <a href="./login/logout.php"><i class="glyphicon glyphicon-off"></i> Logout</a>
-                            </li>
-                        </ul>
-                    </ul>
-                    <a href="#"></a>
+                                <a href="#"><i class="glyphicon glyphicon-off"></i> Logout</a>
+                            </li>                             
+                        </ul>                         
+                    </ul>                     
+                    <a href="#"></a> 
                     <ul class="nav nav-pills nav-stacked">
                         <li class="nav-header"></li>
-                    </ul>
-                </div>
-                <!-- /col-3 -->
-                <div class="col-sm-9">
-                    <!-- column 2 -->
-                    <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> My Dashboard</strong></a>
-                    <hr>
-                    <div class="row">
-                        <!-- center left-->
-                        <div class="col-md-6">
+                    </ul>                     
+                </div>                 
+                <!-- /col-3 -->                 
+                <div class="col-sm-9"> 
+                    <!-- column 2 -->                     
+                    <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> My Dashboard</strong></a> 
+                    <hr> 
+                    <div class="row"> 
+                        <!-- center left-->                         
+                        <div class="col-md-6"> 
                             <div class="btn-group btn-group-justified">
                                 <a href="#" class="btn btn-primary col-sm-3"> <i class="glyphicon glyphicon-plus"></i> <br> New Case </a>
                                 <a href="#" class="btn btn-primary col-sm-3"> <i class="glyphicon glyphicon-cloud"></i> <br> Cloud</a>
                                 <a href="#" class="btn btn-primary col-sm-3"> <i class="glyphicon glyphicon-cog"></i> <br> Settings </a>
                                 <a href="#" class="btn btn-primary col-sm-3"> <i class="glyphicon glyphicon-question-sign"></i> <br> Help</a>
-                            </div>
+                            </div>                             
                             <hr>
-                            <div class="panel panel-default">
+                            <div class="panel panel-default"> 
                                 <div class="panel-heading">
                                     <h4><span class="glyphicon glyphicon-th-list"></span> Recent Cases</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <table class="table table-striped">
+                                </div>                                 
+                                <div class="panel-body"> 
+                                    <table class="table table-striped"> 
                                         <thead>
                                             <tr>
                                                 <th>Name</th>
                                                 <th>Officer</th>
                                                 <th>Status</th>
                                             </tr>
-                                        </thead>
+                                        </thead>                                         
                                         <tbody>
                                             <tr>
                                                 <td>45</td>
@@ -138,38 +138,37 @@
                                                 <td>..</td>
                                                 <td>..</td>
                                             </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <!--/panel-body-->
-                            </div>
-                            <!--/panel-->
-                            <!--tabs-->
-                            <!--/tabs-->
-                        </div>
-                        <!--/col-->
-                        <div class="col-md-6">
+                                        </tbody>                                         
+                                    </table>                                     
+                                </div>                                 
+                                <!--/panel-body-->                                 
+                            </div>                             
+                            <!--/panel-->                             
+                            <!--tabs-->                             
+                            <!--/tabs-->                             
+                        </div>                         
+                        <!--/col-->                         
+                        <div class="col-md-6"> 
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <h4>Welcome</h4>
                                 </div>
                                 <div class="panel-body">
-                                    <p>Welcome to FF84™. If you would like to create a new case plese select "new case"</p>
-                                    <p>Visit the Bootstrap Playground at <a href="http://bootply.com">Bootply</a> to tweak this layout or discover more useful code snippets.</p>
+                                    <p>Welcome to FF84™. To create a new case use the "New Case" button, for further assistance please use the </p>
                                 </div>
-                            </div>
-                            <div class="panel panel-default">
+                            </div>                             
+                            <div class="panel panel-default"> 
                                 <div class="panel-heading">
                                     <h4><span class="glyphicon glyphicon-facetime-video"></span> Camera Feeds</h4>
-                                </div>
-                                <div class="panel-body">
-                                    <table class="table table-striped">
+                                </div>                                 
+                                <div class="panel-body"> 
+                                    <table class="table table-striped"> 
                                         <thead>
                                             <tr>
                                                 <th>Location</th>
                                                 <th>Status</th>
                                             </tr>
-                                        </thead>
+                                        </thead>                                         
                                         <tbody>
                                             <tr>
                                                 <td>45</td>
@@ -179,31 +178,31 @@
                                                 <td>289</td>
                                                 <td>56.2%</td>
                                             </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <!--/panel-body-->
-                            </div>
-                            <!--/panel-->
-                            <!--/panel-->
-                        </div>
-                        <!--/col-span-6-->
-                    </div>
-                    <!--/row-->
-                    <a href="#"></a>
-                    <div class="row">
+                                        </tbody>                                         
+                                    </table>                                     
+                                </div>                                 
+                                <!--/panel-body-->                                 
+                            </div>                             
+                            <!--/panel-->                             
+                            <!--/panel-->                             
+                        </div>                         
+                        <!--/col-span-6-->                         
+                    </div>                     
+                    <!--/row-->                     
+                    <a href="#"></a> 
+                    <div class="row"> 
                         <div class="col-md-12">
                             <ul class="list-group">
 </ul>
-                        </div>
-                    </div>
-                </div>
-                <!--/col-span-9-->
-            </div>
-        </div>
-        <!-- /Main -->
-        <div class="modal" id="addWidgetModal">
-            <div class="modal-dialog">
+                        </div>                         
+                    </div>                     
+                </div>                 
+                <!--/col-span-9-->                 
+            </div>             
+        </div>         
+        <!-- /Main -->         
+        <div class="modal" id="addWidgetModal"> 
+            <div class="modal-dialog"> 
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -216,14 +215,14 @@
                         <a href="#" data-dismiss="modal" class="btn">Close</a>
                         <a href="#" class="btn btn-primary">Save changes</a>
                     </div>
-                </div>
-                <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dalog -->
-        </div>
-        <!-- /.modal -->
-        <!-- script references -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
+                </div>                 
+                <!-- /.modal-content -->                 
+            </div>             
+            <!-- /.modal-dalog -->             
+        </div>         
+        <!-- /.modal -->         
+        <!-- script references -->         
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>         
+        <script src="js/bootstrap.min.js"></script>         
+        <script src="js/scripts.js"></script>         
     </body>
