2ab19422a3d18c363474ad2cb4177c927051dd57
Mr Blint <al844976@wcupa.edu>
2018-02-24 21:51:05 -0500

    newCaseAdded

--- stream.py
+++ stream.py
@@ -4,16 +4,15 @@ url = 'http://ff84.wcupa.network:1337/'
 ip = "rtsp://192.168.1.246:554/11.h264"
 vcap = cv2.VideoCapture(ip)
 count=1
+img_num = 1
 while(1):
     #having the two variables tells vcap.read to unpack the tuple
     ret, frame = vcap.read()
     if(count % 30 == 0):
-        cv2.imwrite(str(count)+".jpg", frame)
-        image = open(str(count)+'.jpg', 'rb')
+        cv2.imwrite(str(img_num)+".jpg", frame)
+        image = open(str(img_num)+'.jpg', 'rb')
         image_raw = image.read()
-        files = {'media': base64.encodestring(image_raw)}
-        requests.post(url, files=files)
+        requests.post(url, files=image_raw)
         #os.remove(str(count)+'.jpg')
     cv2.waitKey(1)
-    count+=1
-    
+    count+=1  
