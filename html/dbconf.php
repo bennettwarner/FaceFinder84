4c1a41142b2df032314b437a74210c47bb6d302c
Bennett Warner <bennettdwarner@gmail.com>
2018-03-02 15:34:25 -0500

    rm pass

--- html/dbconf.php
+++ html/dbconf.php
@@ -2,5 +2,5 @@
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
 $username = "ff84"; // Mysql username
-$password = "xm6829bs"; // Mysql password
+$password = ""; // Mysql password
 $db_name = "ff84"; // Database name
--- html/login/dbconf.php
+++ html/login/dbconf.php
@@ -2,7 +2,7 @@
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
 $username = "ff84"; // Mysql username
-$password = "xm6829bs"; // Mysql password
+$password = ""; // Mysql password
 $db_name = "ff84"; // Database name
 
 //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
